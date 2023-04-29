5ff10eee4c1b2f884057285e1d300bad1b4c93bd
Thiana <thianacantunes@gmail.com>
2023-04-28 22:50:49 -0300

    change router.use

--- src/schemas/auth.schemas.js
+++ src/schemas/auth.schemas.js
@@ -3,14 +3,14 @@ import joi from "joi"
 export const loginSchema = joi.object({
 
     email: joi.string().email().required(),
-    password: joi.string().required()
+    password: joi.number().required()
 }) 
 
 export const userSchema = joi.object({
 
     name: joi.string().required(),
     email: joi.string().email().required(),
-    password: joi.string().required().min(3),
+    password: joi.number().required().min(3),
     state: joi.string().required(),
     city: joi.string().required(),
     road: joi.string().required()
