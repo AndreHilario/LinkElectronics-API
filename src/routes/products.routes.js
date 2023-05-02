7df5fc8fa986d8086cf1e0aadeea14bd4119a602
Andre Hilario <andre18hilario@gmail.com>
2023-05-02 17:54:03 -0300

    Fix: Clean code

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,14 +4,13 @@ import {
   getProducts,
   postProductDetails
 } from '../controllers/products.controller.js'
-import { authValidation } from '../middlewares/auth.middleware.js'
 import { validateSchema } from '../middlewares/validateSchema.middlewares.js'
 import { productDetailsSchema } from '../schemas/product.schema.js'
 
 const productsRouter = Router()
 
 productsRouter.get('/', getProducts)
-// productsRouter.use(authValidation)
+
 productsRouter.get('/products/:id', getProductById)
 productsRouter.post(
   '/shopping/:id',
